2f9f6468d69b5bd7bb80c22e1b383942e8e55950
tokitauhid <tokitauhidmim12@gmail.com>
2026-06-28 21:45:55 +0600

    feat: configure OpenNext for Cloudflare Workers deployment with Supabase env vars

--- .open-next/.build/durable-objects/queue.js
+++ .open-next/.build/durable-objects/queue.js
@@ -180,7 +180,7 @@ var DOQueueHandler = class extends DurableObject {
           "INSERT OR REPLACE INTO sync (id, lastSuccess, buildId) VALUES (?, unixepoch(), ?)",
           // We cannot use the deduplication id because it's not unique per route - every time a route is revalidated, the deduplication id is different.
           `${host}${url}`,
-          "2SuzA4SkFjxHqLszJJM-h"
+          "BEUmu6h7btxZ1-O1vagtK"
         );
       }
       this.routeInFailedState.delete(msg.MessageDeduplicationId);
@@ -232,7 +232,7 @@ var DOQueueHandler = class extends DurableObject {
     }
     this.routeInFailedState.set(msg.MessageDeduplicationId, updatedFailedState);
     if (!this.disableSQLite) {
-      this.sql.exec("INSERT OR REPLACE INTO failed_state (id, data, buildId) VALUES (?, ?, ?)", msg.MessageDeduplicationId, JSON.stringify(updatedFailedState), "2SuzA4SkFjxHqLszJJM-h");
+      this.sql.exec("INSERT OR REPLACE INTO failed_state (id, data, buildId) VALUES (?, ?, ?)", msg.MessageDeduplicationId, JSON.stringify(updatedFailedState), "BEUmu6h7btxZ1-O1vagtK");
     }
     await this.addAlarm();
   }
@@ -256,8 +256,8 @@ var DOQueueHandler = class extends DurableObject {
       return;
     this.sql.exec("CREATE TABLE IF NOT EXISTS failed_state (id TEXT PRIMARY KEY, data TEXT, buildId TEXT)");
     this.sql.exec("CREATE TABLE IF NOT EXISTS sync (id TEXT PRIMARY KEY, lastSuccess INTEGER, buildId TEXT)");
-    this.sql.exec("DELETE FROM failed_state WHERE buildId != ?", "2SuzA4SkFjxHqLszJJM-h");
-    this.sql.exec("DELETE FROM sync WHERE buildId != ?", "2SuzA4SkFjxHqLszJJM-h");
+    this.sql.exec("DELETE FROM failed_state WHERE buildId != ?", "BEUmu6h7btxZ1-O1vagtK");
+    this.sql.exec("DELETE FROM sync WHERE buildId != ?", "BEUmu6h7btxZ1-O1vagtK");
     const failedStateCursor = this.sql.exec("SELECT * FROM failed_state");
     for (const row of failedStateCursor) {
       this.routeInFailedState.set(row.id, JSON.parse(row.data));
